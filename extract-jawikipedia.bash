7f3f74f02d56ecdeea7b6bcbdd86590d3d9f16e0
Takeo Kunishima <t.kunishi@gmail.com>
2020-07-08 17:44:00 +0900

    check if wikiextractor is already downloaded

--- extract-jawikipedia.bash
+++ extract-jawikipedia.bash
@@ -4,5 +4,6 @@ datadir=$HOME/data
 infile=$1
 outdir=${datadir}/extracted-`date +%Y%m%d`
 
-git clone https://github.com/attardi/wikiextractor.git
+[[ ! -d wikiextractor ]] && \
+  git clone https://github.com/attardi/wikiextractor.git
 python3 wikiextractor/WikiExtractor.py -o "${outdir}" "${infile}"
